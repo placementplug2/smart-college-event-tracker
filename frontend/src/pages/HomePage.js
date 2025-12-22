c23ec4cf081344d42d55e8ad41bd5817acbd9961
placementplug2 <placementplug2@gmail.com>
2025-12-22 08:01:31 +0530

    Create HomePage.js

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventList from '../components/EventList';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Upcoming College Events</h1>
+      <EventList />
+    </div>
+  );
+};
+
+export default HomePage;
